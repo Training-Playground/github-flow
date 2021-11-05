61390676edfc5a78049f3e05dbdedbf5c8abc490
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2021-11-05 18:13:26 +0530

    Add Student Test class

---
+++ src/test/java/Student.java
@@ -0,0 +1,58 @@
+public class Student {
+    private String id;
+    private String name;
+    private String address;
+    private String contactNumber;
+
+    public Student() {
+    }
+
+    public Student(String id, String name, String address, String contactNumber) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.contactNumber = contactNumber;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getContactNumber() {
+        return contactNumber;
+    }
+
+    public void setContactNumber(String contactNumber) {
+        this.contactNumber = contactNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                ", contactNumber='" + contactNumber + '\'' +
+                '}';
+    }
+}
